76aaa0320773901fc179114f32bc8f353df6377d
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-12-05 16:16:21 +0800

    removed this. and setState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-// import UserItem from "./components/users/UserItem";
 import Users from "./components/users/Users";
 import User from "./components/users/User";
 import axios from "axios";
@@ -10,8 +9,8 @@ import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import "./App.css";
 
-// convert function App() to class App for now & we need to extend the React.Component (but if we import {Component from above then can just say Component})
 const App = () => {
+  //useState mimics the state below
 
   const [users, setUsers] = useState([]);
   const [user, setUser] = useState({});
@@ -27,105 +26,103 @@ const App = () => {
   //   alert: null
   // };
 
-  searchUsers = async text => {
-    this.setState({ loading: true });
+  const searchUsers = async text => {
+    setLoading(true);
 
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data.items, loading: false });
+    setUsers(res.data.items);
+    setLoading(false);
   };
 
   // Get a single Github user. username is the same as login, which is in User.js and UserItem.js
 
-  getUser = async username => {
-    this.setState({ loading: true });
+  const getUser = async username => {
+    setLoading(true);
 
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ user: res.data, loading: false });
+    setUser(res.data);
+    setLoading(false);
   };
 
   //Get users repo
 
-  getUserRepos = async username => {
-    this.setState({ loading: true });
+  const getUserRepos = async username => {
+    setLoading(true);
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ repos: res.data, loading: false });
+    setRepos(res.data);
+    setLoading(false);
   };
 
   //Clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
-
-  //Set Alert function is passed first to Search.js, which renders the msg and type and is then passed back to App.js
-  //App.js then passes the props msg and type to Alert.js because Search.js and Alert.js cannot pass props between each other
-  setAlert = (msg, type) => {
-    // this just puts the alert into the state so if you try to search with nothing then alert will become an object which will be msg: "please enter message" and type: "light". can also just put alert:{msg, type} and should work
-    this.setState({ alert: { msg: msg, type: type } });
-    setTimeout(() => this.setState({ alert: null }), 5000);
+  const clearUsers = () => {
+    setUsers([]);
+    setLoading(false);
   };
-  // render() is a lifecycle method that runs when all the other components are loaded
-  render() {
-    //All javscript goes under the render() including variables, functions etc.
-    const { users, user, repos, loading } = this.state;
 
-    return (
-      // JSX must only have ONE parent element, otherwise get warning. So everything must go inside this one div. In chrome developer you'll see everything, including the App div is wrapped around a div called "root".
-      //So can replease <di v className="App"><div> with <React.Fragment></React.Fragment>, <Fragment></Fragment> or just <> so that it goes directly from "root" and everything is rendered within
-      // Wrapping everything in Router so we can control which stuff goes where
-      <Router>
-        <div className="App">
-          {/* title is a prop that we will pass down to Navbar.js */}
-          <Navbar />
-          <div className="container">
-            <Alert alert={this.state.alert} />
-            {/* rendering Search and Users into Fragments for the route "/" */}
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={props => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      // if users.length is more than 0 then showClear is true (which comes from Search.js where if true then show button) or else showClear is set to false so no show
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                    {/* loading and users are being passed in as props so that Users.js can access them */}
-                    <Users loading={loading} users={users} />
-                  </Fragment>
-                )}
-              />
-              <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/user/:login"
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={this.getUser}
-                    getUserRepos={this.getUserRepos}
-                    user={user}
-                    repos={repos}
-                    loading={loading}
+  //changed setAlert to "showAlert" because setAlert is already used to set the name of the state above
+
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+
+    setTimeout(() => setAlert(null), 5000);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        {/* title is a prop that we will pass down to Navbar.js */}
+        <Navbar />
+        <div className="container">
+          <Alert alert={alert} />
+          {/* rendering Search and Users into Fragments for the route "/" */}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    // if users.length is more than 0 then showClear is true (which comes from Search.js where if true then show button) or else showClear is set to false so no show
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={showAlert}
                   />
-                )}
-              />
-            </Switch>
-          </div>
+                  {/* loading and users are being passed in as props so that Users.js can access them */}
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" component={About} />
+            <Route
+              exact
+              path="/user/:login"
+              render={props => (
+                <User
+                  {...props}
+                  getUser={getUser}
+                  getUserRepos={getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+                />
+              )}
+            />
+          </Switch>
         </div>
-      </Router>
-    );
-  }
-}
+      </div>
+    </Router>
+  );
+};
 
 export default App;
